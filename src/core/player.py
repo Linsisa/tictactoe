6267a86961893b4163f5934aa27133062bc07460
Linsisa <luca.insisa@gmail.com>
2026-06-03 17:53:53 +0200

    feat: implement Player class with move retrieval based on strategy

---
+++ src/core/player.py
@@ -0,0 +1,25 @@
+"""Player class representing a player in the Tic Tac Toe game."""
+
+from dataclasses import dataclass
+
+from core.strategies import Strategy
+
+
+@dataclass
+class Player:
+    name: str
+    symbol: str
+    strategy: Strategy
+
+    def get_move(self, board: "Board") -> tuple[int, int] | None:
+        """
+        Get the player's move based on their strategy.
+
+        Args:
+            board (Board): The current state of the game board.
+
+        Returns:
+            tuple[int, int] | None: The row and column indices in case of AI playing
+                                    else "None" player move come from UI.        
+        """
+        return self.strategy.get_move(board)
