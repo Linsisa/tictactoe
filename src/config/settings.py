b4c5dbaf4e3a839a0f55340251fe7d3ac844af44
Linsisa <luca.insisa@gmail.com>
2026-06-04 12:17:46 +0200

    refactor: remove unused player name constants from settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,12 +2,10 @@
 
 BOARD_SIZE = 3  # Standard tic tac toe board size (3x3)
 WIN_LENGTH = 3  # Number of symbols in a row, column, diagonal needed to win
+AI_SLEEP_TIME = 2  # Time in seconds to simulate AI thinking time
 
 SYMBOL_X = "X"
 SYMBOL_O = "O"
 EMPTY_CELL = None
-
-HUMAN_PLAYER_NAME = "You"
-AI_PLAYER_NAME = "Computer"
 
 AVAILABLE_SYMBOLS = (SYMBOL_X, SYMBOL_O)
